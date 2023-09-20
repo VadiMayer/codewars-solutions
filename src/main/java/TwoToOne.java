3cd26e21d3bab5081b976e69e9e668a016c42ac5
Vadim <h4998998482@gmail.com>
2023-09-20 13:45:19 +0300

    solution TwoToOne 7 kyu

---
+++ src/main/java/TwoToOne.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class TwoToOne {
+    public static String longest (String s1, String s2) {
+        StringBuilder str = new StringBuilder();
+        str.append(s1);
+        str.append(s2);
+        Character[] uniqueChars = str.chars()
+                .distinct()
+                .mapToObj(c -> (char) c)
+                .toArray(Character[]::new);
+        str.setLength(0);
+        Arrays.sort(uniqueChars);
+        for(int i = 0; i < uniqueChars.length; i++) {
+            str.append(uniqueChars[i]);
+        }
+        return str.toString();
+    }
+}
