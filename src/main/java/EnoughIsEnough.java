1edee70ccd49ea25b246ddb0f0560b79ff430ba0
Vadim <h4998998482@gmail.com>
2023-09-21 17:26:57 +0300

    solution EnoughIsEnough 6 kyu fix

--- src/main/java/EnoughIsEnough.java
+++ src/main/java/EnoughIsEnough.java
@@ -1,23 +1,17 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class EnoughIsEnough {
-
-    public static void main(String[] args) {
-        System.out.println(Arrays.toString(EnoughIsEnough.deleteNth(new int[] { 1, 1, 1, 1, 1 }, 5)));
-    }
-
     public static int[] deleteNth(int[] elements, int maxOccurrences) {
         List<Integer> photos = new ArrayList<>();
-        for(int num: elements) {
+        for (int num : elements) {
             photos.add(num);
         }
         List<Integer> newPhotos = new ArrayList<>();
-        for(int i = 0; i < photos.size(); i++) {
+        for (int i = 0; i < photos.size(); i++) {
             int count = 0;
             if (newPhotos.contains(photos.get(i)) && maxOccurrences >= 1) {
-                for(int j = 0; j < photos.size(); j++) {
+                for (int j = 0; j < photos.size(); j++) {
                     try {
                         if (photos.get(i).equals(newPhotos.get(j))) {
                             count++;
@@ -26,7 +20,6 @@ public class EnoughIsEnough {
                             break;
                         }
                     } catch (IndexOutOfBoundsException e) {
-                        count++;
                         continue;
                     }
                 }
