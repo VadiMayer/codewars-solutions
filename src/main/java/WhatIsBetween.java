172d352cc1c472d4b09675c6955e0080a726ab8f
Vadim <h4998998482@gmail.com>
2023-09-28 21:16:18 +0300

    solution WhatIsBetween 8kyu

---
+++ src/main/java/WhatIsBetween.java
@@ -0,0 +1,13 @@
+public class WhatIsBetween {
+    public static int[] between(int a, int b) {
+        int size = 0;
+        for(int i = a; i <= b; i++) {
+            size++;
+        }
+        int[] numbers = new int[size];
+        for(int i = 0; i < size; i++) {
+            numbers[i] = a + i;
+        }
+        return numbers;
+    }
+}
