c3c53e27bafc1ac94b9afb312dbcfa6c6c0d03ca
Vadim <h4998998482@gmail.com>
2023-09-28 21:14:52 +0300

    solution FakeBinary 8kyu

---
+++ src/main/java/FakeBinary.java
@@ -0,0 +1,14 @@
+public class FakeBinary {
+    public static String fakeBin(String numberString) {
+        char[] chars = numberString.toCharArray();
+        StringBuilder str = new StringBuilder();
+        for(int i = 0; i < chars.length; i++) {
+            if (chars[i] < '5') {
+                str.append(0);
+            } else if (chars[i] >= '5') {
+                str.append(1);
+            }
+        }
+        return str.toString();
+    }
+}
