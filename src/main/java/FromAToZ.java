84df923e0dd3a9ff7031fe6ebbbf8e8244ef53a2
Vadim <h4998998482@gmail.com>
2023-09-28 21:20:17 +0300

    solution FromAToZ 8kyu

---
+++ src/main/java/FromAToZ.java
@@ -0,0 +1,12 @@
+public class FromAToZ {
+    public static String gimmeTheLetters(String s){
+        String[] arrayStr = s.split("-");
+        StringBuilder lettersInLine = new StringBuilder();
+        int start = (int) arrayStr[0].charAt(0);
+        int end = (int) arrayStr[1].charAt(0);
+        for(int i = start; i <= end; i++) {
+            lettersInLine.append((char) i);
+        }
+        return lettersInLine.toString();
+    }
+}
